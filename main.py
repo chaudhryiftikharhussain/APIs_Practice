4b82caee467c0ba082a531b9c26da25743ff3f75
Chaudhry Iftikhar Hussain <chaudhryiftikharhussain@outlook.com>
2026-09-02 13:40:39 +0500

    initial commit

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 from fastapi import FastAPI, status
 from starlette.responses import JSONResponse
 from utils import read_employee_names_json_file
@@ -6,6 +8,7 @@ app = FastAPI()
 
 @app.get("/return-message")
 def read_root():
+    a = 4 / 0
     return {"message": "Hello World"}
 
 
@@ -17,9 +20,10 @@ def read_item(number: int):
 @app.get("/return-all-employee-names")
 def return_all_employees_names():
     print("/return-all-employee-names api is called")
-    employee_names = read_employee_names_json_file()
+    file_read_ok, employee_names, message = read_employee_names_json_file()
     print("now returning all employee names")
     return employee_names
+
 
 @app.get("/check-employee/{name}")
 def check_employee(name: str):
@@ -65,3 +69,53 @@ def check_employee(name: str):
                 "name_exist": name_exist
             }
         )
+
+
+
+@app.get("/search-books/{author_name}")
+def search_books_by_name(author_name: str, category: str | None = None):
+    try:
+        print(f"/search-books api is called with author_name {author_name}, - {category}")
+
+        with open("books_v1.json", "r") as file:
+            books = json.load(file)
+
+        author_names_in_db = [b['author_last_name'] for b in books]
+        print(author_names_in_db)
+        if author_name not in author_names_in_db:
+            return JSONResponse(
+                status_code=404,
+                content={
+                    "message": "author does not exist",
+                }
+            )
+
+        print(author_name)
+        print(books)
+
+        author_books = []
+        for book in books:
+            if book["author_last_name"] == author_name:
+                print("book found")
+                if category is not None and category == book["category"]:
+                    print("category found")
+                    author_books.append(book)
+                else:
+                    author_books.append(book)
+
+        return JSONResponse(
+            status_code=200,
+            content={
+                "message": "books found",
+                "author_books": author_books
+            }
+        )
+    except Exception as e:
+        print("exception occured")
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content={
+                "message": f"server error {e}"
+            }
+        )
+
